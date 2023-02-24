5dc4e52191c4d98f6bc060928d79b65b06170cb4
Your Namecl <you@example.com>
2023-02-24 10:43:52 +0100

    trying to commit

--- src/components/Starpage/Startpage.tsx
+++ src/components/Starpage/Startpage.tsx
@@ -5,6 +5,7 @@ export const Startpage = () => {
     return(<>
         <div className="main-img">
             <h1>Movie night!</h1>
+            <p>Find your favorite movie</p>
         </div>
     </>);
 }
